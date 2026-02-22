1933893d871ee15b6baf9ece4c164932b5167705
Samal <56509582+FransoArbela@users.noreply.github.com>
2026-02-22 12:08:15 +0100

    chore: clean up unused code and files across the project

--- src/pages/DetailedProject.tsx
+++ src/pages/DetailedProject.tsx
@@ -9,21 +9,22 @@ const DetailedProject = () => {
 
 	return (
 		<div className="bg-white p-4">
-            <button type="button"
-                onClick={() => {
-                    navigator.clipboard.writeText(window.location.href);
-                    const notification = document.createElement('div');
-                    notification.innerText = "URL copied to clipboard!";
-                    notification.className = "fixed bottom-4 right-4 bg-purple-500 text-white p-2 rounded";
-                    document.body.appendChild(notification);
-                    setTimeout(() => {
-                        document.body.removeChild(notification);
-                    }, 3000);
-                }}
-                className="mb-4 px-4 py-2 bg-purple-800 text-white rounded hover:bg-purple-900 cursor-pointer"
-            >
-                Copy URL
-            </button>
+			<button
+				type="button"
+				onClick={() => {
+					navigator.clipboard.writeText(window.location.href)
+					const notification = document.createElement("div")
+					notification.innerText = "URL copied to clipboard!"
+					notification.className = "fixed bottom-4 right-4 bg-purple-500 text-white p-2 rounded"
+					document.body.appendChild(notification)
+					setTimeout(() => {
+						document.body.removeChild(notification)
+					}, 3000)
+				}}
+				className="mb-4 px-4 py-2 bg-purple-800 text-white rounded hover:bg-purple-900 cursor-pointer"
+			>
+				Copy URL
+			</button>
 			{project ? (
 				<div className="flex flex-col justify-center items-center gap-4 min-h-screen ">
 					<div className="w-full max-w-3xl mx-auto shadow-lg p-2">
--- src/projectData.ts
+++ src/projectData.ts
@@ -1,4 +1,3 @@
-
 const projects = [
 	{
 		id: 1,
