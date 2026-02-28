7d078db795f2b4281228141add6b46931b0d6aa8
Samal <56509582+FransoArbela@users.noreply.github.com>
2026-02-28 19:31:18 +0100

    refactor: simplify Contact page layout and remove unused navigation functionality

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,16 +1,16 @@
-import { useNavigate } from "react-router-dom"
-import { Button } from "#src/components/index.js"
-
 export default function Contact() {
-	const navigate = useNavigate()
-
 	return (
-		<div className="flex flex-col justify-center items-center bg-bg min-h-screen">
-			<h1 className="mb-4 font-bold text-text">Contact Page</h1>
-			<p className="mb-8 text-accent">Get in touch with us.</p>
-			<Button variant="primary" onClick={() => navigate("/")} className="px-6 py-3">
-				Back to Home
-			</Button>
+		<div className="flex flex-col justify-center items-center min-h-screen ">
+			<div className="bg-white p-8 rounded-md shadow-md w-full max-w-lg flex flex-col gap-6">
+				<h1 className="mb-4 font-bold text-text">Contact Page</h1>
+				<p className="mb-8 text-accent">Get in touch.</p>
+				<ul className="text-lg flex flex-col gap-2 bg-gray-100 p-8 rounded-md ">
+					<li>samal.ibrahim1999@gmail.com</li>
+					<li>+47 465 98 356</li>
+					<li>LinkedIn: <a href="https://linkedin.com/in/samal-ibrahim" target="_blank" rel="noopener noreferrer" className="hover:text-accent">linkedin.com/in/samal-ibrahim</a></li>
+					<li>GitHub: <a href="https://github.com/samal-ibrahim" target="_blank" rel="noopener noreferrer" className="hover:text-accent">github.com/samal-ibrahim</a></li>
+				</ul>
+			</div>
 		</div>
 	)
 }
